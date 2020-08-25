c3535d4f96cf79e9c4150ca280f5905ba25fad5a
Andres Pacheco <apacheco73@hotmail.com>
2020-08-25 15:02:42 -0300

    Base operations in Player service

--- src/player/player.service.ts
+++ src/player/player.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Player } from './player.entity';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+  constructor(
+    @InjectRepository(Player)
+    private readonly playerRepository: Repository<Player>,
+  ) { }
+
+  getPlayer(id: number): Promise<Player> {
+    return this.playerRepository.findOne(id);
+  }
+
+  getPlayers(): Promise<Player[]> {
+    return this.playerRepository.find();
+  }
+
+  createPlayer(player: Player): Promise<Player> {
+    return this.playerRepository.save(player);
+  }
+
+  updatePlayer(player: Player): Promise<Player> {
+    const playerInDb = this.getPlayer(player.Id);
+    if (playerInDb) {
+      return this.playerRepository.save(player);
+    }
+  }
+
+  async deletePlayer(id: number): Promise<Player> {
+    const playerInDb = await this.getPlayer(id);
+    if (playerInDb) {
+      return this.playerRepository.remove(playerInDb);
+    }
+  }
+}
