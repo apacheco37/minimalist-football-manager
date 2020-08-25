d742eee53dfabec667c162097061036db73c0707
Andres Pacheco <apacheco73@hotmail.com>
2020-08-25 13:52:49 -0300

    Fix Position enum

--- src/player/player.entity.ts
+++ src/player/player.entity.ts
@@ -2,16 +2,16 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 import { Team } from "src/team/team.entity";
 
 enum Position {
-  GK,
-  DFL,
-  DFC,
-  DFR,
-  ML,
-  MC,
-  MR,
-  FL,
-  FC,
-  FR
+  GK = 'GK',
+  DFL = 'DFL',
+  DFC = 'DFC',
+  DFR = 'DFR',
+  ML = 'ML',
+  MC = 'MC',
+  MR = 'MR',
+  FL = 'FL',
+  FC = 'FC',
+  FR = 'FR',
 }
 
 @Entity()
